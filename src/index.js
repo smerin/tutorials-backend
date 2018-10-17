ef9ad6f0260cd1e5df0c7b143dda8d19065cf88e
George Smerin <gsmerin@gmail.com>
2018-10-17 07:48:11 +0100

    Add graphql yoga server

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.express.use(cookieParser());
+
+// Decode the jwt so we can get the user ID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // Put the userId onto the request for further requests to access
+    req.userId = userId;
+  }
+  next();
+});
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+    port: process.env.PORT,
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
